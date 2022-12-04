45124bb83473192507d980421724dd4ab5cb0df9
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-03 21:19:13 -0500

    Update IonImplant.py

--- IonImplant.py
+++ IonImplant.py
@@ -4,12 +4,13 @@ import numpy as np
 class IonImplant(inputgrid: SimulationGrid_3D, potential: Potential):
   time: int
   
-  #Random Number Generator; due to random kinetic motion; up to
-  #ten units of distance per time stamp
-  a = random.randrange(-10,10)
 
-  #Add a bias due to coulombic repulsion
+  #Pseudo-random movement of points subject to some potential
   for i in range(len(inputgrid.points.keys())):
+    #Random Number Generator; due to random kinetic motion; up to
+    #ten units of distance per time stamp
+    random_kinetic = [random.randrange(-10,10),random.randrange(-10,10),random.randrange(-10,10)]
+    
     for j in range(len(inputgrid.points.keys())):
       #no repulsion from self
       if i == j:
@@ -18,7 +19,7 @@ class IonImplant(inputgrid: SimulationGrid_3D, potential: Potential):
         point1 = inputgrid.points(i)
         point2 = inputgrid.points(j)
         distance = np.sqrt((point1[0]-point2[0])**2+(point1[1]-point2[1])**2+(point1[2]-point2[2])**2)
-        force = potential.force(1,1,distance)
+        force = int(potential.force(1,1,distance))
         
       
       
