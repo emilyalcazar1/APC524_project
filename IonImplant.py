c219d8f69b15805aa84e7aa3b99b89c65c425fa7
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-03 21:24:17 -0500

    Update IonImplant.py

--- IonImplant.py
+++ IonImplant.py
@@ -19,7 +19,12 @@ class IonImplant(inputgrid: SimulationGrid_3D, potential: Potential):
         point1 = inputgrid.points(i)
         point2 = inputgrid.points(j)
         distance = np.sqrt((point1[0]-point2[0])**2+(point1[1]-point2[1])**2+(point1[2]-point2[2])**2)
-        force = int(potential.force(1,1,distance))
+        
+        force_mag = int(potential.force(1,1,distance))
+        force_dir12 = np.array([point1[0]-point2[0],point1[1]-point2[1],point1[2]-point2[2]])/distance
+        force_dir12norm = np.round(force_dir12).astype(int)
+        force12 = force_mag * force_dir12norm
+        
         
       
       
