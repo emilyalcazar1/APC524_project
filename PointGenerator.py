28f3a9c249e0ea5856ada05065aada77bbbcd97b
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-07 16:04:18 -0500

    Update PointGenerator.py
    added square implementation; needs commenting

--- PointGenerator.py
+++ PointGenerator.py
@@ -1,6 +1,6 @@
 def PointGenerator(npoints: int, init_condition, xlim = 100, ylim = 100, zlim = 100):
     err_flag = 0;
-    valid_param = ["random"]
+    valid_param = ["random", "line","square"]
     grid = {}
     
     print(init_condition)
@@ -33,3 +33,29 @@ def PointGenerator(npoints: int, init_condition, xlim = 100, ylim = 100, zlim = 
             z = random.randrange(0,zlim)
             grid[str(i)] = [x,y,z]
         return grid
+    
+    if init_condition == "line":
+        for i in range(npoints):
+            grid[str(i)] = [0,0,i]
+        return grid
+    
+    if init_condition == "square":
+        maxlen = int(npoints**0.5)
+        excess = npoints-maxlen**2
+        
+        for i in range(maxlen):
+            for j in range(maxlen):
+                grid[str(i*maxlen+j)] = [i,j,0]
+        
+        
+        if excess > (maxlen + 1):
+            for i in range(maxlen + 1):
+                grid[str(maxlen*(maxlen+1)+i)] = [maxlen,i,0]
+            for i in range(excess - maxlen - 1):
+                grid[str(maxlen**2 + i)] = [i,maxlen,0]
+            return grid
+            
+        else: 
+            for i in range(excess):
+                grid[str(maxlen**2 + i)] = [maxlen,i,0]
+            return grid
