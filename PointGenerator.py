38605d4074894e7b756522fdc287b0b05178e41c
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-07 15:53:44 -0500

    Update PointGenerator.py
    added "random" implementation

--- PointGenerator.py
+++ PointGenerator.py
@@ -1,12 +1,14 @@
-def PointGenerator(npoints, init_condition):
+def PointGenerator(npoints: int, init_condition, xlim = 100, ylim = 100, zlim = 100):
     err_flag = 0;
-    valid_param = []
+    valid_param = ["random"]
     grid = {}
     
-    if isinstance(init_condition) != list:
+    print(init_condition)
+    
+    if not isinstance(init_condition,list):
         err_flag = 1
     
-    if isinstance(init_condition) == str:
+    if isinstance(init_condition, str):
         err_flag = 2
         for i in range(len(valid_param)):
             if init_condition == valid_param[i]:
@@ -19,7 +21,15 @@ def PointGenerator(npoints, init_condition):
         if err_flag == 2:
             raise TypeError("Input argument string for initial condition does not exist.")
     
-    if isinstance(init_condition) == list:
+    if isinstance(init_condition,list):
         for i in range(len(init_condition)):
             grid[str(i)] = init_condition[i]
+        return grid
+    
+    if init_condition == "random":
+        for i in range(npoints):
+            x = random.randrange(0,xlim)
+            y = random.randrange(0,ylim)
+            z = random.randrange(0,zlim)
+            grid[str(i)] = [x,y,z]
         return grid
