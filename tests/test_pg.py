c16d6d961e0fcce9ae7dd1f9917f9ce53c112949
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-08 12:49:13 -0500

    Update test_pg.py
    updated capitalization syntax for convention

--- tests/test_pg.py
+++ tests/test_pg.py
@@ -1,27 +1,27 @@
-import PointGenerator
+import point_generator
 
 def PassingTest_List():
-    assert PointGenerator(2,[[0,0,0],[1,1,1]]) == {'0':[0,0,0],'1':[1,1,1]}
+    assert point_generator(2,[[0,0,0],[1,1,1]]) == {'0':[0,0,0],'1':[1,1,1]}
     
 def PassingTest_ListSquare():
-    assert PointGenerator(2,[[0,0,0],[0,1,0],[1,0,0],[1,1,0]]) == PointGenerator(4,'square')
+    assert point_generator(2,[[0,0,0],[0,1,0],[1,0,0],[1,1,0]]) == point_generator(4,'square')
 
 def PassingTest_Line():
-    assert PointGenerator(2,'line') == {'0':[0,0,0],'1':[0,0,1]}
+    assert point_generator(2,'line') == {'0':[0,0,0],'1':[0,0,1]}
     
 def PassingTest_Square():
-    assert PointGenerator(4,'square') == {'0':[0,0,0],'1':[0,1,0],'2':[1,0,0],'3':[1,1,0]}
+    assert point_generator(4,'square') == {'0':[0,0,0],'1':[0,1,0],'2':[1,0,0],'3':[1,1,0]}
 
 def FailingTest_ListDim():
     try:
-        PointGenerator(2,[[0,0,0],[1,1]])
+        point_generator(2,[[0,0,0],[1,1]])
         assert False
     except TypeError:
         assert True
 
 def FailingTest_UndefInit():
     try:
-        PointGenerator(2,'undef')
+        point_generator(2,'undef')
         assert False
     except SyntaxError:
         assert True
