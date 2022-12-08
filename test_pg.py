e88e0fcd0e633127a0ecf207cd4daf4f6fe48d05
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-07 23:51:04 -0500

    Create test_pg.py

---
+++ test_pg.py
@@ -0,0 +1,24 @@
+import PointGenerator
+
+def PassingTest_List():
+    assert PointGenerator(2,[[0,0,0],[1,1,1]]) == {'0':[0,0,0],'1':[1,1,1]}
+
+def PassingTest_Line():
+    assert PointGenerator(2,'line') == {'0':[0,0,0],'1':[0,0,1]}
+    
+def PassingTest_Square():
+    assert PointGenerator(4,'square') == {'0':[0,0,0],'1':[0,1,0],'2':[1,0,0],'3':[1,1,0]}
+
+def FailingTest_ListDim():
+    try:
+        PointGenerator(2,[[0,0,0],[1,1]])
+        assert False
+    except TypeError:
+        assert True
+
+def FailingTest_UndefInit():
+    try:
+        PointGenerator(2,'undef')
+        assert False
+    except SyntaxError:
+        assert True
