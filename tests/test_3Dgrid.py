b7ed9ed61a1b77023dfa498cc1df39b757e880eb
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-13 17:52:16 -0500

    Update and rename test_3Dgrid.py to tests/test_3Dgrid.py

---
+++ tests/test_3Dgrid.py
@@ -0,0 +1,54 @@
+import SimulationGrid_3D
+
+def PassingTest():
+    try:
+        points = {'a':[1,1,1],'b':[2,2,2]}
+        SimulationGrid_3D(3,3,3,points)
+        assert True
+    except:
+        assert False
+    
+# This test should fail since the input type for the points is not a dict
+def Failing_IncorrectPoints1():
+    try:
+        points = [[0,0,0],[1,1,1]]
+        SimulationGrid_3D(3,3,3,points)
+        assert False
+    except TypeError:
+        assert True
+    
+# This test should fail as a is out of bounds above in z
+def Failing_OutOfBoundsAbove():
+    try:
+        points = {'a':[1,1,4],'b':[2,2,2]}
+        SimulationGrid_3D(3,3,3,points)
+        assert False
+    except:
+        assert True
+
+# This test should fail as the coordinates are not lists
+def Failing_IncorrectDictInput():
+    try:
+        points = {'a':'1,2,3','b':(2,3,4)}
+        SimulationGrid_3D(3,3,3,points)
+        assert False
+    except TypeError:
+        assert True
+    
+# This test should fail as the coordinates are not 3D
+def Failing_IncorrectDictInputDim():
+    try:
+        points = {'a':[1,2],'b':[1,2,3,4]}
+        SimulationGrid_3D(3,3,3,points)
+        assert False
+    except TypeError:
+        assert True
+
+# This test should fail as a is out of bounds below in z
+def Failing_OutOfBoundsBelow():
+    try:
+        points = {'a':[1,1,-1],'b':[2,2,2]}
+        SimulationGrid_3D(3,3,3,points)
+        assert False
+    except:
+        assert True
