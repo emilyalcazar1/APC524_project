cc98e1182a85bd17995294ec50d6bf33f55454ba
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-13 17:25:46 -0500

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,5 @@ import nox
 @nox.session
 def tests(session: nox.Session) -> None:
     session.install(".[test]")
-    test_files = ['test_3Dgrid.py']
+    test_files = ['../tests/test_3Dgrid.py']
     session.run('pytest', *test_files)
