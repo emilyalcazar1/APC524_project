c9ce4e99a6b9f0e5b366af318e5a3189bc0b39db
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-14 13:14:49 -0500

    Update test_pg.py

--- tests/test_pg.py
+++ tests/test_pg.py
@@ -2,26 +2,26 @@ import sys
 sys.path.insert(1, 'src/simcode')
 from point_generator import point_generator
 
-def PassingTest_List():
+def test_PassingTest_List():
     assert point_generator(2,[[0,0,0],[1,1,1]]) == {'0':[0,0,0],'1':[1,1,1]}
     
-def PassingTest_ListSquare():
+def test_PassingTest_ListSquare():
     assert point_generator(2,[[0,0,0],[0,1,0],[1,0,0],[1,1,0]]) == point_generator(4,'square')
 
-def PassingTest_Line():
+def test_PassingTest_Line():
     assert point_generator(2,'line') == {'0':[0,0,0],'1':[0,0,1]}
     
-def PassingTest_Square():
+def test_PassingTest_Square():
     assert point_generator(4,'square') == {'0':[0,0,0],'1':[0,1,0],'2':[1,0,0],'3':[1,1,0]}
 
-def FailingTest_ListDim():
+def test_FailingTest_ListDim():
     try:
         point_generator(2,[[0,0,0],[1,1]])
         assert False
     except TypeError:
         assert True
 
-def FailingTest_UndefInit():
+def test_FailingTest_UndefInit():
     try:
         point_generator(2,'undef')
         assert False
