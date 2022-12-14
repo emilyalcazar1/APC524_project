db27c9d50bc918b3493c0671e31f998832fa13e4
Joshy2019187 <63621941+Joshy2019187@users.noreply.github.com>
2022-12-14 13:18:09 -0500

    Update noxfile.py
    Updated to generalize to run all tests

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,4 @@ import nox
 @nox.session
 def tests(session: nox.Session) -> None:
     session.install("pytest")
-    #session.install("-r","SimulationGrid_3D.py")
-    test_files = ['tests/test_3Dgrid.py', 'tests/test_pg.py']
     session.run("pytest",*session.posargs)
