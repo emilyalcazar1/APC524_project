4f422d03c2f7a9cca1d423ccd53ba28bcc9b0eef
Mpbland <45463267+Mpbland@users.noreply.github.com>
2022-12-15 10:43:41 -0500

    Update and rename test_CVDsim.py to test_CVD_Simulation.py

--- tests/test_CVD_Simulation.py
+++ tests/test_CVD_Simulation.py
@@ -1,18 +1,25 @@
 import sys
 sys.path.insert(1, 'src/simcode')
-from CVDsim import CVDsim
+from CVD_Simulation import CVD_Simulation
 
-def test_PassingTest():
+def test_PassingTest1():
     try:
-        CVDsim(5,10,10)
+        CVD_Simulation(5,10,10)
         assert True
     except:
         assert False
-    
+        
+def test_PassingTest2():
+    try:
+        CVD_Simulation(10,25,15)
+        assert True
+    except:
+        assert False 
+        
 # This test should fail because there are zero sites being modeled
 def test_Failing_NoSites():
     try:
-        CVDsim(0,10,10)
+        CVD_Simulation(0,10,10)
         assert False
     except TypeError:
         assert True
