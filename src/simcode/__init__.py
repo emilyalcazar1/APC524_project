a96454f894fb9526ca0a1e51d2035efac745b407
emilyalcazar1 <105386513+emilyalcazar1@users.noreply.github.com>
2022-12-15 14:24:22 -0500

    Adding imports to __init__

--- src/simcode/__init__.py
+++ src/simcode/__init__.py
@@ -3,4 +3,5 @@ from .Potentials import *
 from .SimulationGrid_3D import *
 from .point_generator import *
 from .CVD_Simulation import *
+from .Random_num_gen_1D import *
 import matplotlib.pyplot as plt
