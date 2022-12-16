7b2548a443e223ee477f2c968767bbd8a0e7ec52
Mpbland <45463267+Mpbland@users.noreply.github.com>
2022-12-15 19:57:33 -0500

    Update CVD_Simulation_Plots.py
    Generalized CVD_Simulation_Plots to accept data array

--- src/simcode/CVD_Simulation_Plots.py
+++ src/simcode/CVD_Simulation_Plots.py
@@ -1,28 +1,30 @@
-"""
-This file contains code to plot two figures to better
-quantify the output of CVD_Simulation. The first figure
-is a plot of the overall surface activity of the system
-over time, and the second figure is a plot of the average
-height of the system versus time.
-"""
+def CVD_Simulation_Plots(data): 
 
-# Run CVD_Simulation 
-data = CVD_Simulation(5,25,10) 
+    """
+    This function plots two figures to better quantify the
+    output of CVD_Simulation. The first figure is a plot of 
+    the overall surface activity of the system over time, and 
+    the second figure is a plot of the average height of the
+    system versus time.
+    """
+  
+    # Aquire time from data array
+    time = data[4]
+    
+    # Acquire average height from data array
+    aveh = data[5]
 
-# Save data as tot
-tottime = data[1]
-time = data[4]
-aveh = data[5]
-totact = data[6]
+    # Acquire total activity from data array
+    totact = data[6]
 
-# Plot the overall surface activity versus time
-plt.figure(1)
-plt.plot(time[:-1],totact[:-1],'red')
-plt.xlabel('Time')
-plt.ylabel('Overall Surface Activity')
+    # Plot the overall surface activity versus time
+    plt.figure(1)
+    plt.plot(time[:-1],totact[:-1],'red')
+    plt.xlabel('Time')
+    plt.ylabel('Overall Surface Activity')
 
-# Plot the average height versus time
-plt.figure(2)
-plt.plot(time[:-1],aveh[:-1],'red')
-plt.xlabel('Time')
-plt.ylabel('Average Height')
+    # Plot the average height versus time
+    plt.figure(2)
+    plt.plot(time[:-1],aveh[:-1],'red')
+    plt.xlabel('Time')
+    plt.ylabel('Average Height')
